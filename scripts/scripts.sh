1d5e3d34d9d56cfed815642f6f7f46ca9463c341
khari2453 <121078939+khari2453@users.noreply.github.com>
2025-09-29 21:49:29 +0530

    Update scripts.sh

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -5,7 +5,7 @@ set -e
 ACCOUNT_ID=385122037205
 REGION=us-east-1
 REPO_NAME=brain-task-app
-IMAGE_URI=$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:latest
+IMAGE_URI=385122037205.dkr.ecr.us-east-1.amazonaws.com/brain-task-app:latest
 
 echo "Updating k8s deployment with image $IMAGE_URI"
 
